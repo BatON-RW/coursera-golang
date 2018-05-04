d7e7295378fb49b9a58c19a9e23c753d920ac97e
Novogenin Anton <anton.novozhenin@litota.ru>
2018-05-04 19:02:37 +0400

    Simple solution

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 	"sort"
-	"strings"
+	"strconv"
 )
 
 func main() {
@@ -23,10 +23,10 @@ func main() {
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	return dirTreeAcc(out, path, printFiles, 0)
+	return dirTreeAcc(out, path, printFiles, "")
 }
 
-func dirTreeAcc(out io.Writer, path string, printFiles bool, ident int) error {
+func dirTreeAcc(out io.Writer, path string, printFiles bool, ident string) error {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -44,21 +44,52 @@ func dirTreeAcc(out io.Writer, path string, printFiles bool, ident int) error {
 		return fileInfo[i].Name() < fileInfo[j].Name()
 	})
 
+	if !printFiles {
+		fileInfo = filter(fileInfo, func(f os.FileInfo) bool {
+			return f.IsDir()
+		})
+	}
+
 	for i, f := range fileInfo {
+		isLast := i+1 == len(fileInfo)
+
 		if f.IsDir() {
-			fmt.Fprintln(out, formatStr(f.Name(), ident, i+1 == len(fileInfo)))
-			dirTreeAcc(out, path+"/"+f.Name(), printFiles, ident+1)
-		} else if printFiles {
-			fmt.Fprintln(out, formatStr(f.Name(), ident, i+1 == len(fileInfo)))
+			fmt.Fprintln(out, formatStr(f.Name(), ident, isLast))
+			if isLast {
+				dirTreeAcc(out, path+"/"+f.Name(), printFiles, ident+"\t")
+			} else {
+				dirTreeAcc(out, path+"/"+f.Name(), printFiles, ident+"│\t")
+			}
+
+		} else {
+			fmt.Fprintln(out, formatStrFile(f.Name(), f.Size(), ident, isLast))
 		}
 	}
 	return nil
 }
 
-func formatStr(name string, ident int, isLast bool) string {
+func formatStr(name string, ident string, isLast bool) string {
 	leaf := "├───"
 	if isLast {
 		leaf = "└───"
 	}
-	return strings.Repeat("│\t", ident) + leaf + name
+	return ident + leaf + name
+}
+
+func formatStrFile(name string, size int64, ident string, isLast bool) string {
+	s := "empty"
+	if size > 0 {
+		s = strconv.FormatInt(size, 10) + "b"
+	}
+	return formatStr(name, ident, isLast) + " (" + s + ")"
+}
+
+func filter(vs []os.FileInfo, f func(os.FileInfo) bool) []os.FileInfo {
+	vsf := make([]os.FileInfo, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
 }
